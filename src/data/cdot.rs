4b574a1851eb2af8d78bc61abc11b63d2685f53f
Zeqian Li <zeqianli.bio@gmail.com>
2026-03-28 15:00:05 -0700

    add alias lookup in CdotMapper.contig_index

--- src/data/cdot.rs
+++ src/data/cdot.rs
@@ -710,6 +710,77 @@ impl CdotMapper {
             }
         }
 
+        // Duplicate chr__ notation in contig_index keys
+        let refseq_chrom_aliases: Vec<(&str, &str)> = match genome_build {
+            "GRCh38" => [
+                ("NC_000001.11", "chr1"),
+                ("NC_000002.12", "chr2"),
+                ("NC_000003.12", "chr3"),
+                ("NC_000004.12", "chr4"),
+                ("NC_000005.10", "chr5"),
+                ("NC_000006.12", "chr6"),
+                ("NC_000007.14", "chr7"),
+                ("NC_000008.11", "chr8"),
+                ("NC_000009.12", "chr9"),
+                ("NC_000010.11", "chr10"),
+                ("NC_000011.10", "chr11"),
+                ("NC_000012.12", "chr12"),
+                ("NC_000013.11", "chr13"),
+                ("NC_000014.9", "chr14"),
+                ("NC_000015.10", "chr15"),
+                ("NC_000016.10", "chr16"),
+                ("NC_000017.11", "chr17"),
+                ("NC_000018.10", "chr18"),
+                ("NC_000019.10", "chr19"),
+                ("NC_000020.11", "chr20"),
+                ("NC_000021.9", "chr21"),
+                ("NC_000022.11", "chr22"),
+                ("NC_000023.11", "chrX"),
+                ("NC_000024.10", "chrY"),
+                ("NC_012920.1", "chrM"),
+            ]
+            .to_vec(),
+
+            "GRCh37" => [
+                ("NC_000001.10", "chr1"),
+                ("NC_000002.11", "chr2"),
+                ("NC_000003.11", "chr3"),
+                ("NC_000004.11", "chr4"),
+                ("NC_000005.9", "chr5"),
+                ("NC_000006.11", "chr6"),
+                ("NC_000007.13", "chr7"),
+                ("NC_000008.10", "chr8"),
+                ("NC_000009.11", "chr9"),
+                ("NC_000010.10", "chr10"),
+                ("NC_000011.9", "chr11"),
+                ("NC_000012.11", "chr12"),
+                ("NC_000013.10", "chr13"),
+                ("NC_000014.8", "chr14"),
+                ("NC_000015.9", "chr15"),
+                ("NC_000016.9", "chr16"),
+                ("NC_000017.10", "chr17"),
+                ("NC_000018.9", "chr18"),
+                ("NC_000019.9", "chr19"),
+                ("NC_000020.10", "chr20"),
+                ("NC_000021.8", "chr21"),
+                ("NC_000022.10", "chr22"),
+                ("NC_000023.10", "chrX"),
+                ("NC_000024.9", "chrY"),
+                ("NC_001807.4", "chrM"),
+                // ("NC_012920.1","chrMT"),
+            ]
+            .to_vec(),
+            _ => vec![],
+        };
+
+        for (refseq_id, alias) in refseq_chrom_aliases {
+            if let Some(transcripts) = mapper.contig_index.get(refseq_id) {
+                mapper
+                    .contig_index
+                    .insert(alias.to_string(), transcripts.clone());
+            }
+        }
+
         mapper
     }
 
